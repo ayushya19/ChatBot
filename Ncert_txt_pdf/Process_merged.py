4fa9cdc166a3ed60e9e5e1bda428c5835f441d6d
Ashish Goswami <ashish.g@holosuit.com>
2021-09-01 19:53:43 +0530

    added sentence based dataset with code

--- Ncert_txt_pdf/Process_merged.py
+++ Ncert_txt_pdf/Process_merged.py
@@ -1,54 +1,69 @@
 import re
 
-# This block of code removes the text between EXERCISES
-# and ADDITIONAL EXERCISES
 
-text = ''
-regex = re.compile('(?s)(?<=EXERCISES).*?(?=ADDITIONAL EXERCISES)')
+def readFile(path):
+    '''
+    reads the .txt file from path
+    '''
+    text = ''
+    with open(path, encoding='utf-8') as f:
+        lines = [line.rstrip() for line in f.readlines()]
 
-with open('MergedText.txt', encoding='utf-8') as f:
-    lines = [line.rstrip() for line in f.readlines()]
+        for line in lines:
+            text += line
+            text += '\n'
 
-    for line in lines:
-        text += line
-        text += '\n'
+    return text
 
-modified_text = re.sub(regex, ' ', text)
-# print(modified_text)
 
-text_file = open("Merged_No_Exercises.txt", "w", encoding='utf-8')
-text_file.write(modified_text)
-text_file.close()
+def removeExercise(text):
+    '''
+    removes the text between exercises and additional exercises.
+    [Note]: Still requires to manually remove the last exercise.
+    '''
+    regex = re.compile('(?s)(?<=EXERCISES).*?(?=ADDITIONAL EXERCISES)')
+    modified_text = re.sub(regex, ' ', text)
+    return modified_text
 
-'''
-After the above code is finished .. had to manually 
-remove the last ADDITIONAL EXERCISE.
 
-This:
+def formSentences(text):
+    '''
+    1. Based on '.' and '?' sentences are joined together.
+    2. Smaller length sentences (<=15) are removed (mostly noise).
+    '''
+    mtext = ''
+    for i in text:
+        if i == '.' or i == '?':
+            mtext += i+'\n'
+        elif i == '\n':
+            mtext += ' '
+        else:
+            mtext += i
 
-EXERCISES ADDITIONAL EXERCISES ADDITIONAL EXERCISES ADDITIONAL EXERCISES ADDITIONAL EXERCISES ADDITIONAL EXERCISES ADDITIONAL EXERCISES ADDITIONAL EXERCISES ADDITIONAL EXERCISES
-Suppose that the electric field part of an electromagnetic wave in
-vacuum is E = {(3.1 N/C) cos [(1.8 rad/m) y + (5.4 × 106 rad/s)t]} î .
-About 5% of the power of a 100 W light bulb is converted to visible
-radiation. What is the average intensity of visible radiation
-Assume that the radiation is emitted isotropically and neglect
-reflection.
-Use the formula λ m T = 0.29 cm K to obtain the characteristic
-temperature ranges for different parts of the electromagnetic
-spectrum. What do the numbers that you obtain tell you?
-Given below are some famous numbers associated with
-electromagnetic radiations in different contexts in physics. State
-the part of the electromagnetic spectrum to which each belongs.
-space).
-levels in hydrogen; known as Lamb shift).
-all space-thought to be a relic of the ‘big-bang’ origin of the
-universe].
-associated with a famous high resolution spectroscopic method
-Answer the following questions:
-Why? astronomy is possible only from satellites orbiting the earth.
-Why? human survival. Why?
-surface temperature be higher or lower than what it is now?
-earth would be followed by a severe ‘nuclear winter’ with a
-devastating effect on life on earth. What might be the basis of
-this prediction?
-'''
+    lst = mtext.split('\n')
+    toRemove = []
+    for i in range(len(lst)):
+        if len(lst[i]) <= 15:
+            toRemove.append(i)
+
+    newlist = [lst[i] for i in range(len(lst)) if i not in toRemove]
+    final = ''
+    for i in newlist:
+        final += i+'\n'
+    return final
+
+
+if __name__ == '__main__':
+    # text = readFile('./Dataset/MergedText_v1.txt')
+    # processedText = removeExercise(text)
+
+    # text_file = open("./Dataset/Merged_No_Exercises_v2.txt", "w", encoding='utf-8')
+    # text_file.write(processedText)
+    # text_file.close()
+
+    text = readFile('./Dataset/Merged_Text_NoExample_v3.txt')
+    processedText = formSentences(text)
+    text_file = open("./Dataset/Merged_with_sentences_v4.txt",
+                     "w", encoding='utf-8')
+    text_file.write(processedText)
+    text_file.close()
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Chat bot used across multiple initiative
 
 ## Knowledge Graph:
 1.  AristoKB can be downloaded from [here](https://allenai.org/data/tuple-kb)
-   
+2.  Code for creating Knowledge Graph using networkx is [here](https://colab.research.google.com/drive/1YUiYhv534F9O1XppNNG_E298G_JvV6Ql?usp=sharing)  
